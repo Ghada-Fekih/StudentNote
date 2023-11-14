c66c08d4d2804e9abddc70174648ce7674f7badd
Ghada-fekih <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-14 10:39:55 +0100

    add code js
    add code js

---
+++ js/min.js
@@ -0,0 +1,12 @@
+const fullName = document.querySelector('.fullName'); //recuperation du champ input fullname
+//recuperation des valeurs des champs de formulaire
+const dateOfBirth = document.querySelector('.dateOfBirth');
+//pour les balises select
+const gender = document.querySelector('select[name="gender"]');
+const task1 = document.querySelector('.task1');
+const task2 = document.querySelector('.task2');
+const task3 = document.querySelector('.task3');
+//recuperer les boutons
+const result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
