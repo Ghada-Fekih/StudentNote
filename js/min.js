6d0e4319ce15dc9f7e252f41374ad06ff882a077
Asma-Bk <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-20 11:43:35 +0100

    ending students note
    add js

--- js/min.js
+++ js/min.js
@@ -1,124 +1,152 @@
-const fullName = document.querySelector('.fullName'); //recuperation du champ input fullname
-//recuperation des valeurs des champs de formulaire
+// querySelector est une methode pour afficher le contenu de l input (récupérer les valeurs des champs du formulaire)
+const fullName = document.querySelector('.fullName');
 const dateOfBirth = document.querySelector('.dateOfBirth');
-//pour les balises select
 const gender = document.querySelector('select[name="gender"]');
 const task1 = document.querySelector('.task1');
 const task2 = document.querySelector('.task2');
 const task3 = document.querySelector('.task3');
-//recuperer les boutons
+//récupérer les bouttons
 const result = document.querySelector('.result');
 const submit = document.querySelector('.submit');
 const reset = document.querySelector('.reset');
-//validation rules fullName (controle de saisie)
+//validation rules fullName
+
+//input full name
 function validateFullName(fullName){
-    // check if full name est vide
-    if(!fullName){
-        return "FullName is required";
+    //check if the full name is required.
+    if (!fullName){
+        return "Full Name is required .";
     }
-    // check the length of the full name
-    if (fullName.length < 3 || fullName.length > 20) {
-        return "FullName must be between 3 and 20 characters long.";
+    //check the length of the full name.
+    if (fullName.length < 3 || fullName.lenght > 20){
+        return "Full Name must be between 3 and 20 characters long.";
     }
-    // check the format of the full name.
-    if (!/^[a-zA-Z]+$/.test(fullName)) {
-       return "FullName must contain only letters and spaces.";
+    //check the format of the full name.
+    if (!/^[a-zA-Z ]+$/.test(fullName)){
+        return "Full Name must contain only letters and spaces";
     }
-    // if the fullName passes all of the validation rules,return null
-    return null
-}
-function validateDate(date) {
-    //check if the date is required.
-    if (!date) {
-        return "date is required";
-    }
-    // check the format of the date.
-    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
-    return "date must be in the format yyyy-mm-dd";
+    //if the full name passes all of the validations rules , return null.
+    return null;
 }
 
-//
-const today = new Date();
-const minDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-const maxDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
-if(date < minDate || date > maxDate){
-    return "date must be between today and one year from today"
+//input date
+
+function validateDate(date){
+    //check if the date is required.
+if (!date){
+    return "date is required";
 }
+if (!/^\d{4}-\d{2}-\d{2}$/.test(date)){
+    return "Date must be in the format DD-MM-YYYY";
+}
+    //check the range of the date
+    const today = new Date();
+    const minDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const maxDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+
+    if (date < minDate || date > maxDate){
+        return "Date must be between today and one year from today.";
+    }
+    //if the date passes all the validation rules , return null.
 return null;
 }
 function validateGender(gender){
-    if (!gender){
-        return "gender is required";
+    //check if the gender is required.
+    if (!gender) {
+        return "Gender is required"
     }
-    const validGenders = ["Male","Female"];
+    //check if the gender is a valid value.
+    const validGenders = ["Male" , "Female"];
     if (!validGenders.includes(gender)){
-        return "gender must be a valid value";
+        return "Gender must be a valid value.";
     }
+    //if the gender passes all of the validation rules, return null.
     return null;
 }
+
 function validateMarksText(text){
-    // check if the text is required.
+    //check if the text is required
     if (!text){
-        return"Marks text is required"
+        return "Marks text is required.";
     }
-    // check the format of the text
-    if (!/^[0-9]+(\.[0-9])?$/.test(text)){
-        return "Marks text must be a number or a pourcentage. "
+    //check the format of the text .
+    if (!/^[0-9]+(\.[0-9]+)?$/.test(text)){
+        return "Marks text must be a number or a percentage.";
     }
-
-    // check the range of the text.
-    const minMarks = 0 ;
-    const maxMarks = 20 ;
-    if (text < minMarks || text > maxMarks) {
-        return "marks text must be between 0 and 100"
+    //check the range of the text.
+    const minMarks = 0;
+    const maxMarks = 20;
+    if (text < minMarks || text > maxMarks){
+        return "Marks text must be between 0 and 20.";
     }
-    // check the dicimal places of the text
+    //check the decimal places of the text.
     if (text.includes(".") && text.split(".").length - 1 > 2) {
-        return "marks text must have at most two decimal places."
+        return "Marks text must at most two decimal places.";
     }
-    // if the text passes all the validation rules , return null
+    //if the text passes all of the validation rules,
     return null;
 }
 
-// afficher les valeurs des champs
-submit.addEventListener('click', (event)=> {
-    console.log(gender.value)
- if (validateFullName(fullName.value) !== null) {
- alert(validateFullName(fullName.value))
- return;
- }
- if (validateDate(dateOfBirth.value) !== null){
-     alert(validateDate(dateOfBirth.value))
-     return;
- }
- if (validateGender(gender.value) !== null){
-     alert(validateGender(gender.value))
-     return;
- }
- if (validateMarksText(task1.value)!==null){
+//Afficher les valeurs des champs(function calculer).
+submit.addEventListener('click', (event) => {
+    console.log(dateOfBirth.value);
+if (validateFullName(fullName.value) !== null){
+    alert(validateFullName(fullName.value))
+    return;
+}
+if (validateDate(dateOfBirth.value) !== null){
+    alert(validateDate(dateOfBirth.value))
+    return;
+}
+if (validateGender(gender.value) !== null){
+    alert(validateGender(gender.value))
+    return;
+}
+if (validateMarksText(task1.value) !== null){
     alert(validateMarksText(task1.value))
     return;
- }
- if (validateMarksText(task2.value)!==null){
+}
+if (validateMarksText(task2.value) !== null){
     alert(validateMarksText(task2.value))
     return;
 }
-if (validateMarksText(task3.value)!==null){
+if (validateMarksText(task3.value) !== null){
     alert(validateMarksText(task3.value))
-    return;}
-    // calculer le resultat
-    const total = ((parseInt(task1.value) + parseInt(task2.value) + parseInt(task3.value))\3);
-    // afficher le resultat dans le champ result
-    result.innerHTML = 
-    <h1>Full Name :</h1>
-    ${fullName.value}
-    <h1>Birth Date :</h1>
-    ${dateOfBirth.value}
-    <h1>Gender :</h1>
-    ${gender.value}
-    <h1>Average :</h1>
-    ${total}
-    <h1>Mark :</h1>
-    $ {total >= 10 ? "Succeed" : "fail"}
-    ;
-    });|
+    return;
+}
+//calculer le resultat
+const total = ((parseInt(task1.value) + parseInt(task2.value) + parseInt(task3.value))/3);
+//afficher le résultat dans le champ "result"
+result.innerHTML = `
+<h1>Full Name :</h1>
+${fullName.value}
+<h1>Date of Birth :<h1>
+${dateOfBirth.value}
+<h1>Gender :<h1>
+${gender.value}
+<h1>task1 :<h1>
+${task1.value}
+<h1>task2 :<h1>
+${task2.value}
+<h1>task3 :<h1>
+${task3.value}
+<h1>Average :<h1>
+${total}
+<h1>Mark :<h1>
+${total >= 10 ? "Succeed" : "Fail"}
+
+
+`;
+
+});
+reset.addEventListener('click', () => {
+    result.innerHTML = ""
+    fullName.value = ""
+    dateOfBirth.value = ""
+    task1.value = ""
+    task2.value = ""
+    task3.value = ""
+
+    });
+    
+
